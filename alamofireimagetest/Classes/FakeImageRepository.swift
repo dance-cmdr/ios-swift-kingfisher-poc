b1b4ff2aad2dda7122c1bc1c12cfb2d9f94b9024
alexandros <a.spyropoulos@quidco.com>
2016-05-09 17:26:18 +0100

    Add Repository.find() skeleton method and ImageType & CustomError:Errortype

--- alamofireimagetest/Classes/FakeImageRepository.swift
+++ alamofireimagetest/Classes/FakeImageRepository.swift
@@ -6,15 +6,25 @@
 //  Copyright © 2016 Alexandros Spyropoulos. All rights reserved.
 //
 
+import BrightFutures;
+
 struct ImageType {
     let url: String
+    let description: String
+}
+
+struct CustomError: ErrorType {
+    let code: Int
     let description: String
 }
 
 
 class FakeImageRepository {
     
-    func find() -> [ImageType] {
+    func find() -> Future<[ImageType], CustomError> {
+        let promise = Promise<[ImageType], CustomError>()
         
+        
+        return promise.future
     }
 }
